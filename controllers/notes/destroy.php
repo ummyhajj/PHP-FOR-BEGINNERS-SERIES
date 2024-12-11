89715a579cbdaa33a5a5e7dad5462a5fbe63f5e7
ummyhajj <ummyhajj222@gmail.com>
2024-12-11 16:23:07 +0000

    episode 33

--- controllers/notes/destroy.php
+++ controllers/notes/destroy.php
@@ -5,12 +5,12 @@ use Core\Database;
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
-$currentUserId = 1;
+$currentUserId = 1; 
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $note = $db->query('SELECT * FROM notes WHERE id = :id', [
-        'id' => $_GET['id']
+        'id' => $_POST['id']
     ])->findOrFail();
 
     authorize($note['user_id'] === $currentUserId);
